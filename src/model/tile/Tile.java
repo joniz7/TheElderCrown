45e0244c9bf8f40d82938457176589419d56da4b
Digohd <Digohd1337@gmail.com>
2014-02-12 20:25:26 +0100

    oö-o

--- src/model/tile/Tile.java
+++ src/model/tile/Tile.java
@@ -6,8 +6,13 @@ public abstract class Tile extends GraphicalEntity{
 	
 	protected int typeID;
 
+<<<<<<< HEAD
 	public Tile(String imageName) {
 		super(imageName);
+=======
+	public Tile(String folder, String image) {
+		super(folder, image);
+>>>>>>> 786c37832db9a7538963fc0cab6acb41d847989c
 	}
 
 	public int getTypeID() {
