4286f75ab465dc2545bc7c58b2cbf847611239ac
Jonathan <jonathan.orro@gmail.com>
2014-02-18 20:42:17 +0100

    Created the branch and files for intents.

---
+++ src/model/intent/MoveIntent.java
@@ -0,0 +1,13 @@
+package model.intent;
+
+import model.World;
+
+public class MoveIntent implements Intent{
+
+	@Override
+	public void tick(World world) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
