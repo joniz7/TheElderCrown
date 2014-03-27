c1492bcd0e5776b24dc5a50ff24832f4cc27854b
Simon Österberg <blu3dx@gmail.com>
2014-03-27 12:24:18 +0100

    Villager will no longer idle next to an important object, thus blocking the path for others.

--- src/model/villager/intentions/IdleIntent.java
+++ src/model/villager/intentions/IdleIntent.java
@@ -1,6 +1,9 @@
 package model.villager.intentions;
 
+import model.path.FindObject;
+import model.path.criteria.DontBlock;
 import model.villager.Villager;
+import model.villager.intentions.plan.ExplorePlan;
 import model.villager.intentions.plan.IdlePlan;
 import model.villager.intentions.plan.Plan;
 
@@ -18,6 +21,9 @@ public class IdleIntent extends Intent {
 
 	@Override
 	public Plan getPlan(){
+		if(FindObject.getAdjacentObject(villager.getWorld(), new DontBlock(), null, villager.getX(), villager.getY()) != null){
+		return new ExplorePlan(villager);
+		}
 		return new IdlePlan(villager);
 	}
 
