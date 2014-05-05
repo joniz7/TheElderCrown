3c0acd50b0bc413781469da6112b02333f0fa77b
aommm <niklas@niklaslogren.com>
2014-05-05 12:58:32 +0200

    Set #villages to 1

--- src/model/World.java
+++ src/model/World.java
@@ -68,7 +68,7 @@ public abstract class World implements Tickable, TileBasedMap, PropertyChangeLis
 
 	public final int VILLAGER_SPAWN_POS = 40, VILLAGER_COUNT = 2;
 
-	public final int VILLAGER_COUNT_PER_VILLAGE = 2, VILLAGE_COUNT = 5, VILLAGE_DISTANCE = 40, VILLAGE_SIZE = 20;
+	public final int VILLAGER_COUNT_PER_VILLAGE = 2, VILLAGE_COUNT = 1, VILLAGE_DISTANCE = 40, VILLAGE_SIZE = 20;
 	protected ArrayList<Point> villages;
 	
 	// Keep track of when to spawn babies
